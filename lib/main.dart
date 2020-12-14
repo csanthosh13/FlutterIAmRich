ddef661c65e00188f57afcfb232095e6630bce24
Santhosh Chandrasekharan <csanthosh13@gmail.com>
2020-12-14 08:43:15 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+// main is the starting point of all flutter apps
+void main() => runApp(
+      MaterialApp(
+        home: Center(
+          child: Text("Hello World"),
+        ),
+      ),
+    );
